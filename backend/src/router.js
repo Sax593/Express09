6a9adca7676427c16e945ce814e6262343a27b0c
sax59 <waquetv@gmail.com>
2023-01-02 10:54:20 +0100

    Create route for upload avatar

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
@@ -9,5 +15,18 @@ router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 module.exports = router;
